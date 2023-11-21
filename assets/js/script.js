c4bd33062585df5121c62dbf21c778bf6f0e8f5b
ignasvilkas <ignas.pilybas@icloud.com>
2023-11-21 21:44:38 +0000

    Add subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,6 +62,8 @@ function calculateCorrectAnswer() {
     return [operand1 + operand2, "addition"];
   } else if ( operator === "x") {
     return [operand1 * operand2, "multiply"];
+  } else if(operator === "-") {
+    return [operand1 - operand2, "subtract"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -92,8 +94,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
 
-  document.getElementById("operand1").textContent = operand1;
-  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
   document.getElementById("operator").textContent = "-"
 
 }
